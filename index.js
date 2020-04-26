3953e078505fae084abcf589c929aae13017d014
Nick Booth <nick.booth52@gmail.com>
2020-04-26 13:33:42 +0100

    Significant improvements to download feature, adding better support for DDLs and browser DLs

--- index.js
+++ index.js
@@ -1,19 +1,76 @@
 const fetch = require("node-fetch");
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const AbortController = require("abort-controller");
 
 const config = require("./config.json");
 
 const app = express();
 
+const UA = "DokiDokiModManager/ModClubProxy";
+
 app.use((request, response, next) => {
-   response.set("Access-Control-Allow-Origin", "*");
-   next();
+    response.set("Access-Control-Allow-Origin", "*");
+    next();
 });
+
+app.use(rateLimit({
+    windowMs: 10 * 60 * 1000,
+    max: 20
+}));
+
+app.get("/testddl", (request, response) => {
+    if (!request.query.id) {
+        response.status(400).json({"error": "invalid request"});
+        return;
+    }
+
+    fetch("https://www.dokidokimodclub.com/api/mod/?modID=" + request.query.id, {
+        headers: {
+            "Authorization": "Api-Key " + config.api_key,
+            "User-Agent": UA
+        }
+    }).then(res => res.json()).then(res => {
+        if (res[0]) {
+            const abc = new AbortController();
+            const timeout = setTimeout(() => abc.abort(), 5000);
+            fetch(res[0].modUploadURL, {
+                method: "HEAD",
+                headers: {
+                    "User-Agent": UA
+                },
+                signal: abc.signal
+            }).then(res2 => {
+                const type = res2.headers.get("content-type");
+
+                response.json({
+                    url: res[0].modUploadURL,
+                    type,
+                    downloadable: !!type.match(/^application\/.+/)
+                })
+            }).catch(() => {
+                response.json({
+                    url: res[0].modUploadURL,
+                    type: "unknown",
+                    downloadable: false
+                })
+            }).finally(() => {
+                clearTimeout(timeout);
+            });
+        } else {
+            response.status(404).json({"error": "mod not found"});
+        }
+    }).catch(err => {
+        console.warn(err);
+        response.status(500).json({"error": "an error occurred"});
+    });
+})
 
 app.get("/listing", (request, response) => {
     fetch("https://www.dokidokimodclub.com/api/mod/", {
         headers: {
-            "Authorization": "Api-Key " + config.api_key
+            "Authorization": "Api-Key " + config.api_key,
+            "User-Agent": UA
         }
     }).then(res => res.json()).then(res => {
         response.json(res);
